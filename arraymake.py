2252de4a268e1102d42ecfc64abcc68e93896267
Moka <82600424+PecoPeterson@users.noreply.github.com>
2023-01-12 20:58:16 +0100

    🐱‍🚀 Function which finds the most common char in merged list.
    not fully working, still have to add some stuff 🧡

--- arraymake.py
+++ arraymake.py
@@ -11,6 +11,22 @@ def get_word_lengths(filename):
 
 def merge_list(listname):
   return "".join(listname)
+n = 0
+def user_most_common_check(most_common_letter_ranking):
+  for string in most_common_letter_ranking:
+    string = most_common_in_merged_list[n]
+    string = string[0]
+    user_response = input('Is %s in your word? (True/False)' % (string))
+    if user_response == "False":
+        n ++ 1
+        continue
+    elif user_response == "True":
+        print("Thanks for the info! <3")
+        n = 0
+        break
+    else:
+        print("Invalid input. Please enter True or False.")
+  
 
 word_and_wordlength = get_word_lengths('words.txt')
 
@@ -28,23 +44,13 @@ while True:
 
 filtered_wordlist_withtuples = [(x, y) for x, y in word_and_wordlength if y == user_wordlength]
 filtered_wordlist = [tuple[0] for tuple in filtered_wordlist_withtuples]
-print(filtered_wordlist)
+#print(filtered_wordlist)
 #Reaching this point is required for each time the program is used
 
 merged_list = merge_list(filtered_wordlist)
 c = Counter(merged_list)
 most_common_in_merged_list = c.most_common(26) #needed to rank the most common letters in merged list
 
-user_most_common_check = False
-while user_most_common_check == False:
-  try:
-    most_common_in_merged_list = most_common_in_merged_list[0]
-    most_common_in_merged_list = most_common_in_merged_list[0] #this part is kinda messy (LEONEL MESSI!!! WOOO) I feel, but it works
-    user_most_common_check = input('Is %s in your word?' % (most_common_in_merged_list))
-    user_most_common_check = bool(user_most_common_check)
-    if True:
-      break
-    else: #has to take the next most common letter in most_common_in_merged_list
-      most_common_in_merged_list
-  except ValueError:
-    print('Invalid input: Please enter a valid boolean :>')
+#print(most_common_in_merged_list)
+
+user_most_common_check(most_common_in_merged_list)
