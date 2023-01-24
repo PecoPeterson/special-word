dd8e079aee2a321145061445c25d80303e65c8f1
Moka <82600424+PecoPeterson@users.noreply.github.com>
2023-01-24 23:14:02 +0100

    🔭 deleted old files
    old files and test files deleted, one still stays just to test code
    on small scale

--- aa.py
+++
@@ -1,7 +0,0 @@
-def flatten_and_set(original_list):
-    result = [set(x[0]) for x in original_list]
-    return "".join([str(elem) for sublist in result for elem in sublist])
-
-original_list = [("head", 4), ("dead", 4), ("lead", 4)]
-result = flatten_and_set(original_list)
-print(result)
